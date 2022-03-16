32bf23680836496ee5727ec3869cfb1767e9e762
hudir <hudirybw@gmail.com>
2022-03-16 10:36:33 +0100

    03161030

---
+++ 03.16_array_higher_order_methods/index.js
@@ -0,0 +1,87 @@
+function higherOrder(greeting) {
+    // greeting(parm) is the Callback Function
+    return greeting;
+};
+
+higherOrder(() => 'Good Morning');
+
+//-------#####-------------
+// Array prototype {
+//     Map(){}
+//     filter(){}
+// }
+
+// Function
+
+function fn() { };
+
+// Method
+
+let obj = {
+    fnMethod() {
+
+    }
+};
+
+
+
+
+// Map() method creates a new array with the results of calling a provided function on every ele in the calling
+
+                //   element form the arr |
+// const result = arr.map(function(item, index, array) {
+    // return the new value instead of item
+// })
+// when we need to iterate and return the data for each element - we can use map.
+
+let arr_1 = [1, 2, 3, 4, 5];
+
+// add 2 for each element in the arr_1
+// console.log(arr_1.map((item) => item + 2))  // [ 3, 4, 5, 6, 7 ] with arrow function
+
+// use map to deep clone arr_1
+
+let deepCloneArr_1 = arr_1.map((item) => item); // we have hidden return
+// console.log(deepCloneArr_1);  //[ 1, 2, 3, 4, 5 ]
+
+// Square of the numbers (number ** 2)
+let callback = (ele) => {
+    // for (let ele of arr_1)
+    return ele **2;  // update element and push it
+};
+// console.log(arr_1.map(callback)); // [ 1, 4, 9, 16, 25 ]
+
+// Square of the numbers (number ** 2) in one line
+let suqare = arr_1.map((item) => item ** 2)
+// console.log(suqare); // [ 1, 4, 9, 16, 25 ]
+
+// Square of the numbers (number ** 2) with for loop
+let store_1 = [];
+for (let ele of arr_1) {
+    store_1.push(ele ** 2);
+};
+// console.log(store_1);  // [ 1, 4, 9, 16, 25 ]
+
+// item and index
+//                                         index of element
+let mapContent = ['A', 'B', 'C'].map((ele, index) => {
+    return index > 0 ? `${ele}:${index+1}` : null;
+});
+// console.log(mapContent); // [ null, 'B:2', 'C:3' ]
+// Map always returns an array with the same length of the original. for example if you don't write return it will return an array of undifined but with the same length , If we want to reduce the length of the original array we will use Filter
+
+// 2) passing a function to map
+
+let names = ['Alex', 'Uri', 'Franko'];
+
+let sayHalloArray = names.map(function (name, index) {
+    return `ID: ${index + 1} - Hallo ${name}`;
+});
+// console.log(sayHalloArray);  // [ 'ID: 1 - Hallo Alex', 'ID: 2 - Hallo Uri', 'ID: 3 - Hallo Franko' ]
+let callbackGreeting = function(ele, i) {
+    return `${i + 1} - Hallo ${ele}`;
+}
+
+let sliceAndGreeting = names.slice(1).map(callbackGreeting);
+// console.log(sliceAndGreeting); //  [ '1 - Hallo Uri', '2 - Hallo Franko' ]
+
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# javaScript
+# javaScript Class Notes
+
 
 ![img](./Screenshot%202022-03-16%20at%2009.24.19.png)
+
 ![img](Screenshot.png)
