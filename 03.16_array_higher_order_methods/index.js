6eb880ab1d06ae121a95574379d31db5c1c0d457
hudir <hudirybw@gmail.com>
2022-03-16 15:15:32 +0100

    03161515

--- 03.16_array_higher_order_methods/index.js
+++ 03.16_array_higher_order_methods/index.js
@@ -115,3 +115,117 @@ const reformat_map = (ele, i) => {
     return output;
 };
 // console.log(newArray.map(reformat_map));  // [ { '1': 10 }, { '2': 20 }, { '3': 30 } ]
+
+let blogKeys = ['title', 'description', 'views'];
+let blogValues = ['JavaScript', 'Blog description', 10];
+let blogObj = {
+    [blogKeys[0]] : blogValues[0],
+    [blogKeys[1]] : blogValues[1],
+    [blogKeys[2]] : blogValues[2]
+};
+// console.log(blogObj);
+
+let emptyBlogObj = {};
+emptyBlogObj[blogKeys[0]] = blogValues[0];
+emptyBlogObj[blogKeys[1]] = blogValues[1];
+emptyBlogObj[blogKeys[2]] = blogValues[2];
+// console.log(emptyBlogObj);
+
+let blogData = [{key: 'title', value: 'JavaScript'}, {key: 'description', value: 'Blog description'}, {key: 'views', value: 10}];
+// let output_blog = {};
+// for (let obj of blogData) {
+//     output_blog[obj.key] = obj.value;
+// };
+
+// let output_blog = {
+//     [blogData[0].key]: blogData[0].value,
+//     [blogData[1].key]: blogData[1].value,
+//     [blogData[2].key]: blogData[2].value
+// }
+ emptyBlogObj = {};
+let output_blog = blogData.map((el) => {
+
+    emptyBlogObj[el.key] = el.value
+    return emptyBlogObj;
+});
+
+// console.log(output_blog);
+
+const anStr = "L 2 S 6 L 3 S 4";
+let arr_2 = anStr.split(' ');
+let arr_2Map = arr_2.map((ele) => {
+   if (isNaN(ele)) return ele;  // if we use the condition like if (typeof +(ele) === 'number) it will convert all ele into number directly
+   else return ele * 2;
+}).join(' ');
+// console.log(arr_2Map); // L 4 S 12 L 6 S 8
+
+
+
+
+
+
+
+// --------------------- forEach() ------------------
+// forEach() method executes a provided function once for each array element.
+// forEach() return nothing!
+// return Statement Inside forEach make no Sense
+// forEach() is just like a for of loop
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let oddNumbersStore = [];
+
+// numbers.forEach((num) => {
+//     if (num % 2 !== 0) {
+//         oddNumbersStore.push(num);
+//     };
+// })
+numbers.forEach((num) => {
+    num % 2 !== 0 ? oddNumbersStore.push(num) : null;
+})
+// console.log(oddNumbersStore); // [ 1, 3, 5, 7 ]
+
+// with map
+oddNumbersStore = [];
+let mapOutput = numbers.map(function(num){
+    num % 2 !== 0 ? oddNumbersStore.push(num) : null;
+});
+// console.log(oddNumbersStore); // [ 1, 3, 5, 7 ]
+
+mapOutput = numbers.map((num) => {
+    return num % 2 !== 0 ? num : null;
+});
+// console.log(mapOutput);
+
+
+
+
+
+
+
+
+
+// --------------------- filter() ------------------
+// the filter() method creates a new array, containing elements from an old array which have passed some kind of 'test' / check
+// the test which we give for the elements to 'pass' is provided as a function.
+// filter() does not change the original array.
+
+let arr = [1,2,3,4,5,6,7,8];
+let newArr = arr.filter(placeholder => placeholder % 2 == 0);
+// console.log(newArr); // [ 2, 4, 6, 8 ]
+
+function getOddNumbers(arr) {
+    return arr.filter(function(num) {
+        return num % 2 !== 0; 
+    });
+};
+// console.log(getOddNumbers(arr)); // [ 1, 3, 5, 7 ]
+// console.log(getOddNumbers([99, 3, 0, 11, 9]));  // [ 99, 3, 11, 9 ]
+
+let findWordWithAn = arr => {
+    let arrNew = arr.filter(ele => {
+       return ele.includes('an') 
+    });
+    return arrNew
+};
+console.log(findWordWithAn(['banana', 'cherry', 'apple', 'mango', 'orange', 'kiwi'])); //[ 'banana', 'mango', 'orange' ]
