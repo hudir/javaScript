2d36248138d3118b3948db800e08578c8b145db0
hudir <hudirybw@gmail.com>
2022-04-23 21:56:43 +0200

    nice code form diala

---
+++ PlayGroud/clashcode.js
@@ -0,0 +1,28 @@
+
+
+// sss = 'to the vast qui...'.split(' ').sort((a,b)=>a.length-b.length);
+// console.log(sss);
+// if(sss.every((x,i)=>{
+//     if (i<sss.length-1 && x.length+1!=sss[i+1].length) return false
+//     else return true
+// })) console.log(sss.join(' '))
+// else console.log('ERROR')
+
+
+
+//  const N = 2
+//  if(N===0) console.log(0)
+//  else {
+//      var inputs = '-10 -10'.split(' ').sort((a,b)=>Math.abs(a)-Math.abs(b));
+ 
+//      console.log(inputs);
+     
+//      if (Math.abs(inputs[0])==Math.abs(inputs[1])) {
+//          if(inputs[1]>0) console.log(inputs[1])
+//          else console.log(inputs[0])
+//      }
+//      else console.log(inputs[0])
+//  }
+
+
+
